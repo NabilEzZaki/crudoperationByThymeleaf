bb60508b6b238d5571da7fd02f32c51e5ab8eace
Zaki Nabil <liban1@gmx.com>
2023-10-26 23:14:54 +0200

    Initial commit

---
+++ src/main/java/com/sbThymeleaf/crudoperationByThymeleaf/CrudoperationByThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.sbThymeleaf.crudoperationByThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudoperationByThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudoperationByThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbThymeleaf/crudoperationByThymeleaf/CrudoperationByThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbThymeleaf.crudoperationByThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudoperationByThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
